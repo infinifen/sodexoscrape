2d46aea7307f245e0154c397ba6f725ac16eb21b
infinifen <48841404+infinifen@users.noreply.github.com>
2022-10-05 22:45:28 +0200

    forgor about requests

--- scrape.py
+++ scrape.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 
+from restaurant import Restaurant
 
-def restaurant_from_bs4(bs: BeautifulSoup):
+
+def restaurant_from_bs4(bs: BeautifulSoup) -> Restaurant:
     pass
