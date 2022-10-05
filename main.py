9c7e36ce4c09c73c339e03870e48ce946e8ab520
infinifen <48841404+infinifen@users.noreply.github.com>
2022-10-06 00:06:29 +0200

    add tree print

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 from rich import print
+from rich.tree import Tree
 
+from restaurant import Restaurant
 from scrape import restaurant_from_bs4
 
 
@@ -11,10 +13,22 @@ def get_restaurant_html(restaurant_id: int):
     return html
 
 
+def build_restaurant_tree(r: Restaurant) -> Tree:
+    tree = Tree(r.name, style="bold")
+    for cat in r:
+        cat_subtree = tree.add(cat.name, style="not bold green")
+        for meal in cat:
+            meal_str = f'[default]{meal.name}[/] [bright_black]{meal.kind}[/]'
+            cat_subtree.add(meal_str)
+    return tree
+
+
 def main():
     res = get_restaurant_html(36)
     bs = BeautifulSoup(res.text, features="lxml")
-    print(restaurant_from_bs4(bs))
+    restaurant = restaurant_from_bs4(bs)
+    tree = build_restaurant_tree(restaurant)
+    print(tree)
 
 
 if __name__ == '__main__':
